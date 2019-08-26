1971466f710a8f56a5d89f6e4788166d447ee645
umwalidiane <umwalidiane69@gmail.com>
2019-08-26 08:20:26 +0200

    adding function for likes

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,16 +1,16 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-like',
-//   templateUrl: './like.component.html',
-//   styleUrls: ['./like.component.css']
-// })
-// export class LikeComponent implements OnInit {
-//   likes=0
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+  likes=0
   
-//   upVotes(){
-//     this.likes=this.likes+1;
-//   }
+  upVotes(){
+    this.likes=this.likes+1;
+  }
 //   dislikes=0
 //   downVotes(){
 //     this.dislikes=this.dislikes+1;
