9e9b1fd56d414e724269c2f45973c6ad47cc6662
umwalidiane <umwalidiane69@gmail.com>
2019-08-26 08:27:19 +0200

    adding array for quotes and discription

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,22 +1,22 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Quote } from '../quote';
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
-// @Component({
-//   selector: 'app-quote',
-//   templateUrl: './quote.component.html',
-//   styleUrls: ['./quote.component.css']
-// })
-// export class QuoteComponent implements OnInit {
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
   
-//   quotes:Quote[] = [
-//    new Quote(1, 'When I count my blessings,I count you twice.','you are my great blessing',new Date),
-//  new Quote(2,'am good but not angel','No body is good at all',new Date(2019,7,25)),
-//  new Quote(3,'When it rains look for rainbows. When it’s dark look for stars.','there is always a light at the end of the tunnel.',new Date(2019,7,24)),
-//  new Quote(4,'you can totally do this','Nothing is impossible',new Date(2019,7,22)),
-//  new Quote(5,'Feel the fear and do it anyway','Accepting what is happening in your life.',new Date(2019,7,21)),
-//  new Quote(6,'If you are not too long, I will wait here for you all my life.','Never get tired of waiting for you because our life is not too long',new Date(2019,7,23)),
-//  new Quote(7,'Life is like the ocean, it goes up and down.','this life has both times,time for happiness and time for sadness',new Date(2019,7,23))
-//    ];
+  quotes:Quote[] = [
+   new Quote(1, 'When I count my blessings,I count you twice.','you are my great blessing',new Date),
+ new Quote(2,'am good but not angel','No body is good at all',new Date(2019,7,25)),
+ new Quote(3,'When it rains look for rainbows. When it’s dark look for stars.','there is always a light at the end of the tunnel.',new Date(2019,7,24)),
+ new Quote(4,'you can totally do this','Nothing is impossible',new Date(2019,7,22)),
+ new Quote(5,'Feel the fear and do it anyway','Accepting what is happening in your life.',new Date(2019,7,21)),
+ new Quote(6,'If you are not too long, I will wait here for you all my life.','Never get tired of waiting for you because our life is not too long',new Date(2019,7,23)),
+ new Quote(7,'Life is like the ocean, it goes up and down.','this life has both times,time for happiness and time for sadness',new Date(2019,7,23))
+   ];
 //    toggleDetails(index){
     
 //     this.quotes[index].showDescription = !this.quotes[index].showDescription;
