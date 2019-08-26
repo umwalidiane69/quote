4f949d6f1d3ed9ea24d1f5f109721065e87446b6
umwalidiane <umwalidiane69@gmail.com>
2019-08-26 08:34:51 +0200

    adding function for emitting quote

--- src/app/quote-descr/quote-descr.component.ts
+++ src/app/quote-descr/quote-descr.component.ts
@@ -1,21 +1,21 @@
-// import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
-// import { Quote } from '../quote';
-// @Component({
-//   selector: 'app-quote-descr',
-//   templateUrl: './quote-descr.component.html',
-//   styleUrls: ['./quote-descr.component.css']
-// })
-// export class QuoteDescrComponent implements OnInit {
-//   @Input() quote: Quote;
-//   @Output() isReaded = new EventEmitter<boolean>();
-//   quoteDelete(complete:boolean){
-//     this.isReaded.emit(complete);
-//   }
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quote-descr',
+  templateUrl: './quote-descr.component.html',
+  styleUrls: ['./quote-descr.component.css']
+})
+export class QuoteDescrComponent implements OnInit {
+  @Input() quote: Quote;
+  @Output() isReaded = new EventEmitter<boolean>();
+  quoteDelete(complete:boolean){
+    this.isReaded.emit(complete);
+  }
  
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
